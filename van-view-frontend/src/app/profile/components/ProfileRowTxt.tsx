aadf382faf1211fb32b44496b28e883f926c908a
Antonio Gagliardo <ajgagliardo27@gmail.com>
2025-10-14 21:23:12 -0700

    added a bit of style to the profile

--- van-view-frontend/src/app/profile/components/ProfileRowTxt.tsx
+++ van-view-frontend/src/app/profile/components/ProfileRowTxt.tsx
@@ -1,8 +1,8 @@
 export default function ProfileRowTxt({ left, right }) {
   return (
     <>
-      <div className="m-4 flex w-2/2">
-        <div className="flex-1">{left}</div>
+      <div className="m-2 pb-4 flex w-2/2 border-b-1 border-slate-200  ">
+        <div className="flex-1 text-slate-500">{left}</div>
         <div className="flex-1 text-right">{right}</div>
       </div>
     </>
