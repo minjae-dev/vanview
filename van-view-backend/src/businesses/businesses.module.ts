0dd96c47515e20dc1c00ea9d56ae95d577ea943c
minjamie <minjae2246@gamil.com>
2025-10-16 03:19:18 -0700

    Deploy

--- van-view-backend/src/businesses/businesses.module.ts
+++ van-view-backend/src/businesses/businesses.module.ts
@@ -5,11 +5,10 @@ import { BusinessesService } from './businesses.service';
 import { Business } from './entities/business.entity';
 import { BusinessType } from './entities/businessType.entity';
 import { BusinessRepository } from './repositories/business.repository';
-import { BusinessSeedService } from './services/business-seed.service';
 @Module({
   imports: [TypeOrmModule.forFeature([Business, BusinessType])],
   controllers: [BusinessesController],
-  providers: [BusinessesService, BusinessSeedService, BusinessRepository],
-  exports: [BusinessesService, BusinessSeedService],
+  providers: [BusinessesService, BusinessRepository],
+  exports: [BusinessesService],
 })
 export class BusinessesModule {}
